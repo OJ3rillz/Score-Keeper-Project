8ffa36f88f91ff51ae81688075d44d483c56d823
TimmyJoseph <timitrilli@gmail.com>
2018-09-29 22:39:42 +0100

    add score keeper project progress

--- scoreKeeper.js
+++ scoreKeeper.js
@@ -1,10 +1,23 @@
 var p1Button = document.querySelector("#p1");
-var p2Button = document.getElementById("#p1");
+var p2Button = document.getElementById("p2");
 var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
 var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore = 5;
 
 p1Button.addEventListener("click", function(){
+    if(!gameOver){
     p1Score++;
-    console.log(p1Score);
-    h1.textConsole = p1Score;
+    if(p1Score === winningScore){
+       gameOver = true;
+    }
+    p1Display.textContent = p1Score;
+    }
 });
+
+p2Button.addEventListener("click", function(){
+    p2Score++; 
+    p2Display.textContent = p2Score;
+})
